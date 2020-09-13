7e05e60e38f5d8c86f2a9261d2553d4b3857f62d
unknown <mejdiclubiste@gmail.com>
2020-09-13 15:22:38 +0100

    making the project

---
+++ app/src/main/java/com/example/spacex/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spacex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
