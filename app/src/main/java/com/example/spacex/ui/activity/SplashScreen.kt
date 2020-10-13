33af5ddfbb02b175ace9c75f108a6cf25cf120e8
unknown <mejdiclubiste@gmail.com>
2020-10-13 10:44:20 +0100

    link two transition together

--- app/src/main/java/com/example/spacex/ui/activity/SplashScreen.kt
+++ app/src/main/java/com/example/spacex/ui/activity/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.constraintlayout.motion.widget.MotionLayout
 import com.example.spacex.R
 import kotlinx.android.synthetic.main.activity_splash_screen.*
@@ -14,16 +15,35 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
        // showTheMoonManWithMagic()
         Handler().postDelayed({
-            motion.transitionToEnd()
+            startAnimation()
         }, 2000)
     }
 
-    private fun showTheMoonManWithMagic() {
-   /*     moon_man.animate().translationX(0f).translationY(0f).setDuration(2700).start()
-        Handler().postDelayed({
-            startActivity(Intent(this, NewsActivity::class.java))
-            finish()
-        }, 3000)*/
+    private fun startAnimation() {
+        motion.transitionToState(R.id.collapsed)
+        motion.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                Log.d("nice", "onTransitionStarted: ")
+            }
 
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                Log.d("nice", "onTransitionStarted: ")
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startSecondPartAnimation()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+                Log.d("nice", "onTransitionStarted: ")
+            }
+
+        })
     }
+
+    private fun startSecondPartAnimation() {
+        motion.transitionToState(R.id.explode)
+    }
+
+
 }
