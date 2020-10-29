175777a8d28041b7f982250d93053bd336b19882
mejdi <mejdiclubiste@gmail.com>
2020-10-29 01:04:30 +0000

    testing dsl gradle for the first time

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,10 @@
+object Versions{
+ val recyclerView = "1.1.0"
+ val firebase_analytics = "17.5.0"
+
+}
+
+object Deps{
+val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+val firebase_analytics = "com.google.firebase:firebase-analytics-ktx:${Versions.firebase_analytics}"
+}
