26b4341d507d31d19bd68b615f5370c2bf14cb9b
mejdi <mejdiclubiste@gmail.com>
2020-11-10 17:33:04 +0100

    separate some dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,10 +1,16 @@
 object Versions{
  val recyclerView = "1.1.0"
  val firebase_analytics = "17.5.0"
+ val retrofit_converter_gson = "17.5.0"
+ val glide = "4.11.0"
+ val epoxy = "4.1.0"
 
 }
 
 object Deps{
 val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
 val firebase_analytics = "com.google.firebase:firebase-analytics-ktx:${Versions.firebase_analytics}"
+ val retrofit_converter_gson =  "com.squareup.retrofit2:converter-gson:${Versions.retrofit_converter_gson}"
+ val glide =  "com.github.bumptech.glide:glide:${Versions.glide}"
+ val epoxy =  "com.airbnb.android:epoxy:${Versions.epoxy}"
 }
