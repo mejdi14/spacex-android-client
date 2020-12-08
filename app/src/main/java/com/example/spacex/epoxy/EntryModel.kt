44abe5f1e4e03dfff4b73ffbe0bff1075a1f119b
mejdi <mejdiclubiste@gmail.com>
2020-12-08 22:48:22 +0100

    this one looks so cool

--- app/src/main/java/com/example/spacex/epoxy/EntryModel.kt
+++ app/src/main/java/com/example/spacex/epoxy/EntryModel.kt
@@ -2,21 +2,29 @@ package com.example.spacex.epoxy
 
 import android.app.Application
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 import android.os.Parcel
 import android.os.Parcelable
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelClass
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.example.spacex.R
+import com.example.spacex.function.getListColors
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.android.synthetic.main.entry_item.view.*
 import java.lang.System.load
 import kotlin.coroutines.coroutineContext
+import kotlin.random.Random
 import com.airbnb.epoxy.EpoxyModelWithHolder as EpoxyModelWithHolder1
 
 @EpoxyModelClass(layout = R.layout.entry_item)
@@ -42,6 +50,10 @@ abstract class EntryModel() : EpoxyModelWithHolder1<EntryModel.EntryHolder>() {
         holder.launch_name.text = name
         holder.launch_date.text = date
         holder.launch_mission.text = mission
+
+        val background: Drawable = holder.container.background
+        changingBackgroundColor(background, Color.parseColor(getListColors().get(Random.nextInt(0,7))))
+
         /*Glide
             .with(holder.bedge)
             .load(imageUrl)
@@ -51,18 +63,41 @@ abstract class EntryModel() : EpoxyModelWithHolder1<EntryModel.EntryHolder>() {
 
     inner class EntryHolder: EpoxyHolder() {
         lateinit var launch_name: TextView
+        lateinit var container: ConstraintLayout
         lateinit var launch_date: TextView
         lateinit var launch_mission: TextView
         lateinit var bedge: ImageView
 
         override fun bindView(itemView: View) {
             launch_name = itemView.launch_name
+            container = itemView.container
             launch_date = itemView.launch_date
             launch_mission = itemView.mission_name
             bedge = itemView.padge
         }
+
+
     }
 
 
+
+    private fun changeColorForView(view: View, color: String) {
+
+        val background: Drawable = view.background
+        changingBackgroundColor(background, Color.parseColor(color))
+    }
+
+    private fun changingBackgroundColor(background: Drawable, color: Int) {
+        if (background is ShapeDrawable) {
+            // cast to 'ShapeDrawable'
+            background.paint.color = color
+        } else if (background is GradientDrawable) {
+            // cast to 'GradientDrawable'
+            background.setColor(color)
+        } else if (background is ColorDrawable) {
+            // alpha value may need to be set again after this call
+            background.color = color
+        }
+    }
 
 }
--- app/src/main/java/com/example/spacex/ui/activity/SplashScreen.kt
+++ app/src/main/java/com/example/spacex/ui/activity/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.View
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.app.ActivityOptionsCompat
 import com.bumptech.glide.Glide
@@ -49,7 +50,7 @@ class SplashScreen : AppCompatActivity() {
         motion.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
                 Log.d("nice", "onTransitionStarted: ")
-                triangle.animate().rotation(180f).setDuration(2000).start()
+                triangle.animate().rotation(180f).setDuration(9000).start()
             }
 
             override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
@@ -57,6 +58,7 @@ class SplashScreen : AppCompatActivity() {
             }
 
             override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+
              //   startNewsActivity()
                 startTheFuckingParty()
             }
@@ -69,7 +71,105 @@ class SplashScreen : AppCompatActivity() {
     }
 
     private fun startTheFuckingParty() {
+        Handler().postDelayed({
+            hideRedAndShowWhite()
+        }, 1000)
 
+    }
+
+    private fun hideRedAndShowWhite() {
+        runOnUiThread {
+            mars.alpha = 0f
+            //mars.setImageDrawable(R.drawable.round_white_shape)
+            Handler().postDelayed({
+                border_circle.alpha = 0f
+            }, 200)
+
+            Handler().postDelayed({
+               white_cercle.alpha = 1f
+            }, 400)
+            Handler().postDelayed({
+                flag.alpha = 1f
+            }, 700)
+
+            Handler().postDelayed({
+                triangle.alpha = 0f
+                Glide.with(this).load(R.drawable.space_boy).into(gif_image)
+                flag.alpha = 0f
+
+                japan_holder.alpha = 1f
+            }, 1000)
+
+            Handler().postDelayed({
+                border_circle.alpha = 1f
+                japan_holder.alpha = 0f
+
+            }, 1300)
+
+            Handler().postDelayed({
+                white_cercle.alpha = 1f
+
+            }, 1500)
+
+            Handler().postDelayed({
+                big_mars.alpha = 1f
+
+            }, 1700)
+
+            Handler().postDelayed({
+                white_cercle.alpha = 0f
+                mars.alpha = 1f
+                big_mars.alpha = 0f
+                extra_border.alpha = 1f
+                extra_border2.alpha = 1f
+
+
+            }, 1900)
+
+            Handler().postDelayed({
+                white_cercle.alpha = 0f
+                mars.alpha = 1f
+                big_mars.alpha = 0f
+                extra_border.alpha = 0f
+                extra_border2.alpha = 0f
+                triangle.alpha = 1f
+
+            }, 2100)
+
+
+            Handler().postDelayed({
+                triangle.alpha = 0f
+                yellow_cercle.alpha = 1f
+                white_cercle.alpha = 1f
+
+
+            }, 2300)
+
+            Handler().postDelayed({
+                white_cercle.animate().scaleX(50f).scaleY(50f).setDuration(1000).withEndAction {
+                    mars.alpha = 1f
+                    yellow_cercle.alpha = 0f
+                    white_cercle.alpha = 0f
+                    welcome.alpha = 1f
+                    cowboy.alpha = 1f
+                    Glide.with(this).clear(gif_image)
+                    white_cercle.animate().alpha(0f).setDuration(300).withEndAction {
+                        Handler().postDelayed({
+                           startNewsActivity()
+
+
+                        }, 1000)
+                    }.start()
+                }.start()
+
+
+            }, 2500)
+
+
+
+
+
+        }
 
     }
 
